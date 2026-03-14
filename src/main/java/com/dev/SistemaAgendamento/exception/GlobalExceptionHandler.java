c32dc46329125c724e0114c76e962b95d746c89f
Rhuanvss <rhuanvictor1323@gmail.com>
2026-03-13 23:43:31 -0300

    fix: criado o package exception para o handler

--- src/main/java/com/dev/SistemaAgendamento/exception/GlobalExceptionHandler.java
+++ src/main/java/com/dev/SistemaAgendamento/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.dev.SistemaAgendamento.controller;
+package com.dev.SistemaAgendamento.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
